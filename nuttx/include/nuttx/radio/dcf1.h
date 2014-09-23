28756f01b4c77335c40ba808d434de3d5bb3771e
Daniel Laszlo Sitzer <dlsitzer@gmail.com>
2014-09-23 21:52:14 +0200

    DCF1: Added new dcf1.h in include/nuttx/radio/.

---
+++ nuttx/include/nuttx/radio/dcf1.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <nuttx/irq.h>
+
+struct dcf1_lower_s
+{
+  int  (*attach)(FAR const struct dcf1_lower_s *lower, xcpt_t handler);
+  void (*enable)(FAR const struct dcf1_lower_s *lower);
+  void (*disable)(FAR const struct dcf1_lower_s *lower);
+};
