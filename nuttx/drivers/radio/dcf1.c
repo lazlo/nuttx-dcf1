8fc91c15344a04b9d0a51b4cb3b13d9cd7d7523f
Daniel Laszlo Sitzer <dlsitzer@gmail.com>
2014-09-24 00:03:09 +0200

    dcf1: Use our gpio abstraction inside the dcf1 driver.

--- nuttx/drivers/radio/dcf1.c
+++ nuttx/drivers/radio/dcf1.c
@@ -158,17 +158,17 @@ static struct dcf1_dev {
 
 static bool dcf1_read_data_pin(void)
 {
-	return (dev.pinops->read)(dev.gpio_data);
+	return (*dev.pinops->read)(dev.gpio_data);
 }
 
 static void dcf1_write_pon_pin(const bool out)
 {
-	(dev.pinops->write)(dev.gpio_pon, out);
+	(*dev.pinops->write)(dev.gpio_pon, out);
 }
 
 static void dcf1_write_led_pin(const bool out)
 {
-	(dev.pinops->write)(dev.gpio_led, out);
+	(*dev.pinops->write)(dev.gpio_led, out);
 }
 
 /* Turn the module on or off */
@@ -464,7 +464,7 @@ static ssize_t dcf1_read(file_t *filep, FAR char *buf, size_t buflen)
 	/* TODO Read a complete receieved DCF77 message */
 
 	/* TODO Make independent of STM32 GPIO abstraction */
-	*buf = (dev.pinops->read)(dev.gpio_data);
+	*buf = (*dev.pinops->read)(dev.gpio_data);
 
 	return OK;
 }
@@ -501,15 +501,15 @@ void dcf1_init(struct dcf1_gpio_s *pinops, uint32_t datapin,
 	sem_init(&dev.isr_sem, 1, 0);
 
 	/* Setup pins */
-	(dev.pinops->config)(dev.gpio_led);
-	(dev.pinops->config)(dev.gpio_pon);
-	(dev.pinops->config)(dev.gpio_data);
+	(*dev.pinops->config)(dev.gpio_led);
+	(*dev.pinops->config)(dev.gpio_pon);
+	(*dev.pinops->config)(dev.gpio_data);
 
 	/* Set default output levels (receiver and LED off by default) */
 	dcf1_enable(false);
 
 	/* Register handler for ext. interrupt on DCF1_DATA */
-	(dev.pinops->setevent)(dev.gpio_data, true, true, false, dcf1_interrupt);
+	(*dev.pinops->setevent)(dev.gpio_data, true, true, false, dcf1_interrupt);
 
 	/* Fork a process to wait for interrupts to process */
 	task_create("dcf1", 100, 1024, dcf1_procirq, NULL);
